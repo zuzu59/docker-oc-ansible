22031c3a5052d1d453ea1e1bec445d48a789c78f
Christian Zufferey <christian@zufferey.com>
2019-09-04 17:18:55 +0200

    Là, maintenant cela installe le binz ansible ET oc, cela à l'air de bien marcher :-)

--- scripts_img/image_config.sh
+++ scripts_img/image_config.sh
@@ -1,12 +1,12 @@
 
 #!/bin/bash
 # Petit script pour configurer l'image docker ubuntu utilisée pour le container docker
-# zf190904.1614
+# zf190904.1718
 # source: 
 
 /root/ubuntu_config.sh
 /root/add_ubuntu.sh
-#/root/install_ansible.sh
-#/root/install_oc.sh
-#/root/update.sh
+/root/install_ansible.sh
+/root/install_oc.sh
+/root/update.sh
 
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Petit script pour démarrer le binz
-#zf190904.1603
+#zf190904.1657
 # source:
 
 
@@ -9,6 +9,7 @@
 mkdir ./dev
 mkdir ./ssh
 cp ~/.ssh/id_rsa.pub ./ssh/authorized_keys
+cp ~/.ssh/config ./ssh/config
 
 docker-compose up -d
 ./list.sh
@@ -40,16 +41,22 @@ docker-compose down -v --remove-orphans
 
 "
 
-
 echo -e "
-
-On doit se connecter au container avec:
-
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ubuntu@localhost -p 52222
 
 Si on n'a pas mis sa clef ssh pub dans le fichier ./ssh/authorized_keys, il faut utiliser le 'tototutu' comme password !
 Pour se créer facilement une clef ssh, il faut faire:
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+
+Si on veut que sa clef ssh 'suive' dans le container, il faut vérifier qu'elle se trouve bien dans le 'forward agent' avec:
+ssh-add -l
+
+Si elle n'est pas il faut simplement l'ajouter avec:
+ssh-add
+
+
+On doit se connecter au container avec:
+
+ssh -A -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ubuntu@localhost -p 52222
 
 "
 #read -p "press [ENTER]"
